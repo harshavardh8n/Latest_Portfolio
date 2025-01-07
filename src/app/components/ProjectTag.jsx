e5159a27f54f111a3175a791ae254e15920008e9
harshavardh8n <harshavardhangaikwad39@gmail.com>
2025-01-07 20:13:25 +0530

    respnsiveness done

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
   const buttonStyles = isSelected
-    ? "text-white border-primary-500"
-    : "text-[#ADB7BE] border-slate-600 hover:border-white";
+    ? "bg-primary-500 text-white border-primary-500"
+    : "bg-transparent text-[#ADB7BE] border-slate-600 hover:border-white hover:text-white";
+
   return (
     <button
-      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      className={`${buttonStyles} rounded-full border-2 px-4 sm:px-6 py-2 text-base sm:text-lg font-medium transition-all duration-200 ease-in-out`}
       onClick={() => onClick(name)}
     >
       {name}
